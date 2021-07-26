8301219561ab1c1da404f2ca0edb4aa0a260aa81
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2021-07-26 12:30:50 -0400

    finished react fundamentals

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,35 +4,42 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const [inputValue, setInputValue] = React.useState('');
 
-  // 🐨 add the onSubmit handler to the <form> below
+  function handleOnChange(event) {
+    const { value } = event.target;
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+    setInputValue(value.toLowerCase());
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    onSubmitUsername(inputValue);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="name">Username:</label>
+        <input
+          id="name"
+          type="text"
+          value={inputValue}
+          onChange={handleOnChange}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
 function App() {
-  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  function onSubmitUsername(username) {
+    alert(`You entered: ${username}`);
+  }
+
   return <UsernameForm onSubmitUsername={onSubmitUsername} />
 }
 
-export default App
+export default App;
